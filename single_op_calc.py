62ac6c6c603e308f52254acb03b1e32380fe080f
andrievskiyt <tandrievsky@gmail.com>
2021-03-26 18:35:19 +0200

    ok

--- single_op_calc.py
+++ single_op_calc.py
@@ -9,30 +9,41 @@
         Иначе - выводит сообщение 'Bye!' и прекращает свою работу.
 """
 try:
-    n = int(input('Введіть значення n:'))
-    operation = input('Введіть операцію(+,-,*,/):')
-    x = int(input('Введіть числа:'))
-    if operation == '+':
-        for i in range(1, n):
-            i = int(input('Введіть числа:'))
-            result = x + i
-        print('Результат:', result)
-        input("Continue? (Y/n) ")
-    elif operation == '-':
-        for i in range(1, n):
-            i = int(input('Введіть числа:'))
-            i -= i
-        print('Результат:', i)
-    elif operation == '*':
-        for i in range(1, n):
-            i = int(input('Введіть числа:'))
-            i *= i
-        print('Результат:', i)
-    elif operation == '/':
-        i /= i
-        print('Результат:', i)
-    else:
-        print('Не доступна операція!')
+    while True:
+        n = int(input('Введіть значення n:'))
+        operation = input('Введіть операцію(+,-,*,/):')
+        summary = 0
+        mul = 1
+        while n > 0:
+            if operation == '+':
+                for i in range(1, n + 1):
+                    i = int(input('Введіть числа:'))
+                    summary += i
+                print('Результат:', summary)
+            elif operation == '-':
+                for i in range(1, n + 1):
+                    i = int(input('Введіть числа:'))
+                    i -= i
+                print('Результат:', abs(i))
+            elif operation == '*':
+                for i in range(1, n + 1):
+                    i = int(input('Введіть числа:'))
+                    mul *= i
+                print('Результат:', mul)
+            elif operation == '/':
+                for i in range(1, n + 1):
+                    i = int(input('Введіть числа:'))
+                    i /= i
+                print('Результат:', i)
+            else:
+                print('Не доступна операція!')
+            if input("Continue? (Y/n) ") == "Y":
+                break
+            else:
+                print("Bye!")
+                exit()
+        else:
+            print("Невірне значення")
 except ZeroDivisionError:
     print('На нуль ділити не можна!')
 except ValueError:
