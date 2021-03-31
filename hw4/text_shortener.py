fda578d12060036b1c3c1e990a94b02d5cde29ac
andrievskiyt <tandrievsky@gmail.com>
2021-03-31 17:54:12 +0300

    hw4

--- hw4/text_shortener.py
+++ hw4/text_shortener.py
@@ -6,9 +6,24 @@
 
     Например:
 
-    >>> Программа принимает текст(вводится с клавиатуры(с помощью input())),
+    Программа принимает текст(вводится с клавиатуры(с помощью input())),
     форматирует его(удаляет все скобочки(и их содержимое)) и выводит на экран.
 
     Результат:
     Программа принимает текст, форматирует его и выводит на экран.
 """
+count = 0
+string = input("Введіть текст:")
+length = len(string)
+string_format = ""
+if string.count('(') != string.count(')'):
+    print("Невірна кількість дужок")
+else:
+    for i in range(length):
+        if string[i] == "(":
+            count += 1
+        elif count == 0:
+            string_format += string[i]
+        elif string[i] == ")":
+            count -= 1
+    print("Резульатат:" + string_format)
