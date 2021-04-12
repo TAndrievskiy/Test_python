10e2198d4ddbf23be1d6128ce897e6f0ee5dd6ab
andrievskiyt <tandrievsky@gmail.com>
2021-04-12 14:35:43 +0300

    hw7

--- hw7/lucky_ticket.py
+++ hw7/lucky_ticket.py
@@ -6,10 +6,33 @@
 """
 
 
-def is_lucky(ticket_num: int) -> bool:
-    ticket_num = list()
-    centre = len(ticket_num) // 2
-    if sum(ticket_num[:centre]) == sum(ticket_num[centre:]):
+def main():
+    assert is_lucky(1230) is True
+    assert is_lucky(239017) is False
+    assert is_lucky(134008) is True
+    assert is_lucky(15) is False
+    assert is_lucky(2020) is True
+    assert is_lucky(199999) is False
+    assert is_lucky(77) is True
+    assert is_lucky(479974) is True
+    assert is_lucky(4799731) is False
+    assert is_lucky(1379974) is False
+    print("All tests passed successfully!")
+
+
+def is_lucky(ticket_num) -> bool:
+    ticket = str(ticket_num)
+    centre = len(ticket) // 2
+    sum_1 = 0
+    sum_2 = 0
+    if len(ticket) % 2 != 0:
+        print("Unlucky..(")
+        return False
+    for i in ticket[:centre]:
+        sum_1 += int(i)
+    for j in ticket[centre:]:
+        sum_2 += int(j)
+    if sum_1 == sum_2:
         print("Lucky ticket!!!")
         return True
     else:
@@ -17,15 +40,5 @@ def is_lucky(ticket_num: int) -> bool:
         return False
 
 
-assert is_lucky(1230) is True
-assert is_lucky(239017) is False
-assert is_lucky(134008) is True
-assert is_lucky(15) is False
-assert is_lucky(2020) is True
-assert is_lucky(199999) is False
-assert is_lucky(77) is True
-assert is_lucky(479974) is True
-assert is_lucky(4799731) is False
-assert is_lucky(1379974) is False
-
-print("All tests passed successfully!")
+if __name__ == "__main__":
+    main()
